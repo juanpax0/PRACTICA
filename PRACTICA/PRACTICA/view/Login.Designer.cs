96605250ab4fa0b1fc4aa56b14def975d8224ce0
juanpax0 <juanpaxx0@gmail.com>
2016-07-13 02:18:04 -0600

    Sin ningun cambio importante.

--- PRACTICA/PRACTICA/view/Login.Designer.cs
+++ PRACTICA/PRACTICA/view/Login.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace PRACTICA
 {
-    partial class Form1
+    partial class Login
     {
         /// <summary>
         /// Variable del diseñador necesaria.
@@ -30,7 +30,7 @@
         {
             this.components = new System.ComponentModel.Container();
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.Text = "Login";
         }
 
         #endregion
